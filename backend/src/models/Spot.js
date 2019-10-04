621c6b642ac1e50ad2bf172903fa4afd213ce96c
Felipe Guimarães <luiz.guimaraes@dcomp.sor.ufscar.br>
2019-10-03 23:53:34 -0300

    web view finished

--- backend/src/models/Spot.js
+++ backend/src/models/Spot.js
@@ -9,6 +9,16 @@ const SpotSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
     } // referencia pro usuario que criou o spot
+}, {
+    toJSON: {
+        virtuals: true,
+    },
+});
+
+// cria uma url para thumbnail
+// nessa rota serao servida as imagens das empresas (inseridas pelo usuario)
+SpotSchema.virtual('thumbnail_url').get(function() {
+    return `http://localhost:3333/files/${this.thumbnail}`;
 });
 
 module.exports = mongoose.model('Spot', SpotSchema);
