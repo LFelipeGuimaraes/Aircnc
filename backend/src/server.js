bf718b7c9e9f600777c0570cc48838ab344cb6fd
Felipe Guimarães <luiz.guimaraes@dcomp.sor.ufscar.br>
2019-10-06 05:18:24 -0300

    real time notification working only on web view

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,13 +3,38 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack-wrc2z.mongodb.net/aircnc?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+});
+
+// em produção é recomendado armazenar os usuários conectados
+// em um banco de dados performático, como o Redis
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
+
+    connectedUsers[user_id] = socket.id
+    console.log(socket.id);
+});
+
+// middleware que adiciona à requisição o websocket e os usuários conectados
+// para permitir que todas as rotas utilizem o websocket
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
 });
 
 // req.query: acessa query params
@@ -21,4 +46,4 @@ app.use(express.json())
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Alert } from 'react';
+import socketio from 'socket.io-client';
 import { SafeAreaView, ScrollView, StyleSheet, Image, AsyncStorage } from 'react-native';
 
 import SpotList from '../components/SpotList';
@@ -7,6 +8,19 @@ import logo from '../assets/logo.png'
 
 export default function List() {
     const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('user').then(user_id => {
+            const socket = socketio('http://192.168.0.107:3333', {
+                query: { user_id }
+            })
+            
+            socket.on('booking_response', booking => {
+                console.log('recebi aqui')
+                Alert.alert(`Sua reserva em ${booking.spot.company} em ${booking.date} foi ${booking.approved ? 'APROVADA' : 'REJEITADA'}`);
+            });
+        });
+    }, []);
 
     useEffect(() => {
         AsyncStorage.getItem('techs').then(storagedTechs => {
